0d85927158f6e2e0e1b3e7c287436fa76dd810c3
Al Imran Ahmed <al.imran.cse@gmail.com>
2026-02-25 23:42:16 +0100

    New game added

--- resources/views/pages/game/⚡index/index.blade.php
+++ resources/views/pages/game/⚡index/index.blade.php
@@ -8,53 +8,46 @@
 
     <div class="flex flex-col gap-4">
         @forelse($games as $game)
-            <div class="border rounded p-4">
-                <div><flux:link href="{{route('game.show', $game->slug)}}">{{$game->name}}</flux:link></div>
-                <div>{{$game->members->pluck('name')->implode(', ')}}</div>
+        <div class="border rounded p-4">
+            <div>
+                <flux:link href="{{route('game.show', $game->slug)}}">{{$game->name}}</flux:link>
             </div>
+            <div>{{$game->members->pluck('name')->implode(', ')}}</div>
+        </div>
         @empty
             <div class="text-shadow-slate-400">No game played yet</div>
         @endforelse
     </div>
 
     <flux:modal name="start-game" flyout>
-        <div
-            class="space-y-6"
-            x-data="{ members: [''] }"
-        >
+        <div class="space-y-6" x-data="{ members: ['', ''] }">
 
             <flux:heading size="lg">Start a Game</flux:heading>
 
             @if ($errors->any())
-                <div class="text-red-500 text-sm space-y-1">
-                    @foreach ($errors->all() as $error)
-                        <div>{{ $error }}</div>
-                    @endforeach
-                </div>
+            <div class="text-red-500 text-sm space-y-1">
+                @foreach ($errors->all() as $error)
+                <div>{{ $error }}</div>
+                @endforeach
+            </div>
             @endif
 
+            <div>Members</div>
             <template x-for="(member, index) in members" :key="index">
-                <flux:input
-                    label="Member"
-                    placeholder="Name"
-                    x-model="members[index]"
-                />
+                <flux:input x-bind:placeholder="`Member ${index + 1}: Name`" x-model="members[index]" />
             </template>
 
-            <flux:button
-                type="button"
-                @click="members.push('')"
-            >
-                Add More
-            </flux:button>
+            <template x-if="members.length < 20">
+                <button type="button"
+                        class="rounded-full p-1 hover:bg-slate-100 border border-slate-300"
+                        @click="members.push('')">
+                    <flux:icon name="plus" />
+                </button>
+            </template>
 
             <div class="flex">
                 <flux:spacer />
-                <flux:button
-                    type="button"
-                    variant="primary"
-                    @click="$wire.saveMembers(members);"
-                >
+                <flux:button type="button" variant="primary" @click="$wire.saveMembers(members);">
                     Save changes
                 </flux:button>
             </div>
